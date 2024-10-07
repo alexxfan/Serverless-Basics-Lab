e236cdbe537b9fed5d525a7fb0e441f10e2721a0
alexxfan <alexxfan>
2024-10-07 12:24:58 +0100

    Create movies table and seed it.

--- bin/simple-app.ts
+++ bin/simple-app.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SimpleAppStack } from '../lib/simple-app-stack';
 
+
 const app = new cdk.App();
 new SimpleAppStack(app, 'SimpleAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
